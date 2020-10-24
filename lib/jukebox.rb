bd14eae91b86a6893c6200f5bb814e7919fd1f64
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-24 15:07:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,15 +22,14 @@ songs = [
      puts "#{index + 1}. #{song}"
    end
  end
- 
+
 def play(array)
   puts "Please enter a song name or number:"
   input = gets.strip
   array.each_with_index do |song, index|
-    if input == song || input.to_i == index 
+    if input == song || input.to_i == index
       return "Playing #{song}"
     end
   end
   return "Invalid input, please try again"
 end
-
