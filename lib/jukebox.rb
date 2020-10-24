7513802cc8cb398c923e01ac046ffa8dc44f1165
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-24 15:12:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ def play(array)
   puts "Please enter a song name or number:"
   input = gets.strip
   array.each_with_index do |song, index|
-    if input == song || input == index
+    if input == (index + 1)
       return "Playing #{song}"
     end
   end
